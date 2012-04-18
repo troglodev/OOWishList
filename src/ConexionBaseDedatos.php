87be0568ed504033732fa2d5bdc17b0fccb0b054
Troglodyte Developer <troglodev@gmail.com>
2012-04-18 22:48:16 +0200

    Añadido base de datos

---
+++ src/ConexionBaseDedatos.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ConexionBaseDedatos
+ *
+ * @author federico
+ */
+class ConexionBaseDedatos {
+    //put your code here
+}
+
+?>
