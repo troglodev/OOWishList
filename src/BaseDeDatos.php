563585092b444d372bf6d9a9f465d3b7b03b8142
Troglodyte Developer <troglodev@gmail.com>
2012-04-19 23:15:12 +0200

    Añadida funcionalidad update

--- src/BaseDeDatos.php
+++ src/BaseDeDatos.php
@@ -57,7 +57,7 @@ class BaseDeDatos {
 
     public function insert($table, $values, $rows = null) {
 
-        //insert into wishers values (null,'pepitoeldelospalotes','blabla');
+//insert into wishers values (null,'pepitoeldelospalotes','blabla');
         $sql = 'INSERT INTO ' . $table;
         if ($rows != null) {
             $sql .= ' (' . $rows . ')';
@@ -69,7 +69,7 @@ class BaseDeDatos {
                 continue;
             }
             if (is_string($values[$i])) {
-                //Añadir Quote!!!!
+//Añadir Quote!!!!
                 $values[$i] = '"' . $values[$i] . '"';
             }
         }
@@ -87,13 +87,78 @@ class BaseDeDatos {
         }
     }
 
-    public function update() {
-        
+    public function update($table, $rows, $where) {
+
+        $update = 'UPDATE ' . $table . ' SET ';
+        /*
+          echo '#01 //';
+          print_r($where);
+          for ($i = 0; $i < count($where); $i++) {
+          if ($i % 2 != 0) {
+          if (is_string($where[$i])) {
+          if (($i + 1) != null)
+          $where[$i] = '"' . $where[$i] . '" AND ';
+          else
+          $where[$i] = '"' . $where[$i] . '"';
+          }
+          }
+          }
+          echo '#02 //';
+          print_r($where);
+          $where = implode('', $where);
+          echo '#03 //';
+         * */
+
+
+        //  print_r($where);
+
+
+        $keys = array_keys($rows);
+        for ($i = 0; $i < count($rows); $i++) {
+            if (is_string($rows[$keys[$i]])) {
+                $update .= $keys[$i] . '="' . $rows[$keys[$i]] . '"';
+            } else {
+                $update .= $keys[$i] . '=' . $rows[$keys[$i]];
+            }
+
+// Parse to add commas
+            if ($i != count($rows) - 1) {
+                $update .= ',';
+            }
+        }
+        $cad = '';
+        $keys = array_keys($where);
+        for ($i = 0; $i < count($where); $i++) {
+            if (is_string($where[$keys[$i]])) {
+                $cad .= $keys[$i] . '="' . $where[$keys[$i]] . '"';
+            } else {
+                $cad .= $keys[$i] . '=' . $where[$keys[$i]];
+            }
+
+// Parse to add commas
+            if ($i != count($where) - 1) {
+                $cad .= ',';
+            }
+        }
+
+
+
+        $update .= ' WHERE ' . $cad;
+        try {
+            return $this->dbh->exec($update);
+        } catch (PDOException $e) {
+            echo 'Error actualizando registro<br/>';
+            echo 'Codigo: ' . $e->getCode() . '<br/>';
+            echo 'L&iacute;nea: ' . $e->getLine() . '<br/>';
+            echo 'Mensaje: ' . $e->getMessage() . '<br/>';
+            echo 'consulta= _' . $update;
+            return false;
+        }
     }
 
     public function delete($table, $where) {
-        $sql='DELETE FROM '.$table.' ';
-        $sql.='WHERE '.$where;
+        $sql = 'DELETE FROM ' . $table . ' ';
+        $sql.='WHERE ' . $where;
         try {
             return $this->dbh->exec($sql);
         } catch (PDOException $e) {
@@ -103,10 +168,6 @@ class BaseDeDatos {
             echo 'consulta= _' . $sql;
             return false;
         }
-    }
-
-    public function remove() {
-        
     }
 
     public function disconnect() {
